a3d9096116ea45689d616bb878bcf534e03171ea
jeferson300505 <jeferson300505@gmail.com>
2024-05-30 18:48:07 -0500

    start commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { RamdonUser } from './components/RamdonUser';
+
+function App() {
+  return (
+    <div className="App">
+      <RamdonUser/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RamdonUser.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+
+export const RamdonUser = () => {
+  useEffect(()=>{
+    console.log("hola")
+  })
+  return (
+    <div>hola mundo </div>
+  )
+}
